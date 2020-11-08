d4b679575bcff77f4cc4d88fe8fb6e36346713f3
Abayomi <eyitouyoa3@gmail.com>
2020-11-08 13:42:25 -0500

    layout for skills section complete

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,29 +3,77 @@ import React from 'react'
 export default function Skills() {
     return (
         <>
-            <h1>Skills</h1>
             <section id="skills">
-                <div className="frameworks">
-                    <h1>Frameworks & Libraries</h1>
-                    <span>React</span>
-                    <span>Django</span>
-                    <span>Firebase</span>
-                    <span>Bootstrap</span>
-                    <span>Redux</span>
-                </div>
-                <div className="languages">
-                    <h1>Languages</h1>
-                    <span>Javascript</span>
-                    <span>HTML</span>
-                    <span>CSS</span>
-                    <span>Python</span>
-                    <span>Java</span>
-                    <span>C</span>
-                    <span>C++</span>
-                </div>
-                <div className="v-control">
-                    <h1>Version control</h1>
-                    <span>Github</span>
+                <h1 className="heading">Skills</h1>
+                <div className="skills-container">
+                    <div className="frameworks">
+                        <h1>Frameworks & Libraries</h1>
+                        <article>
+                            <i class="fab fa-react"></i>
+                            <br></br>
+                            <span>React</span>
+                        </article>
+                        <article>
+                            <br></br>
+                            <span>Django</span>
+                        </article>
+                        <article>
+                            <br></br>
+                            <span>Firebase</span>
+                        </article>
+                        <article>
+                            <i class="fab fa-bootstrap"></i>
+                            <br></br>
+                            <span>Bootstrap</span>
+                        </article>
+                        <article>
+                            <br></br>
+                            <span>Redux</span>
+                        </article>
+                    </div>
+                    <div className="languages">
+                        <h1>Languages</h1>
+                        <article>
+                            <i class="fab fa-js"></i>
+                            <br></br>
+                            <span>Javascript</span>
+                        </article>
+                        <article>
+                            <i class="fas fa-code"></i>
+                            <br></br>
+                            <span>HTML</span>
+                        </article>
+                        <article>
+                            <i class="fab fa-css3"></i>
+                            <br></br>
+                            <span>CSS</span>
+                        </article>
+                        <article>
+                            <i class="fab fa-python"></i>
+                            <br></br>
+                            <span>Python</span>
+                        </article>
+                        <article>
+                            <i class="fab fa-java"></i>
+                            <br></br>
+                            <span>Java</span>
+                        </article>
+                        <article>
+                            <br></br>
+                            <span>C</span>
+                        </article>
+                        <article>
+                            <span>C++</span>
+                        </article>
+                    </div>
+                    <div className="v-control">
+                        <h1>Version control</h1>
+                        <article>
+                            <i class="fab fa-github"></i>
+                            <br></br>
+                            <span>Github</span>
+                        </article>
+                    </div>
                 </div>
             </section>
         </>
