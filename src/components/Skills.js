6356587575f3ac344861a011fb28b361fb567da2
Abayomi <eyitouyoa3@gmail.com>
2020-11-08 13:48:53 -0500

    Edited the skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
                     <div className="frameworks">
                         <h1>Frameworks & Libraries</h1>
                         <article>
-                            <i class="fab fa-react"></i>
+                            <i className="fab fa-react"></i>
                             <br></br>
                             <span>React</span>
                         </article>
@@ -22,7 +22,7 @@ export default function Skills() {
                             <span>Firebase</span>
                         </article>
                         <article>
-                            <i class="fab fa-bootstrap"></i>
+                            <i className="fab fa-bootstrap"></i>
                             <br></br>
                             <span>Bootstrap</span>
                         </article>
@@ -34,27 +34,27 @@ export default function Skills() {
                     <div className="languages">
                         <h1>Languages</h1>
                         <article>
-                            <i class="fab fa-js"></i>
+                            <i className="fab fa-js"></i>
                             <br></br>
                             <span>Javascript</span>
                         </article>
                         <article>
-                            <i class="fas fa-code"></i>
+                            <i className="fas fa-code"></i>
                             <br></br>
                             <span>HTML</span>
                         </article>
                         <article>
-                            <i class="fab fa-css3"></i>
+                            <i className="fab fa-css3"></i>
                             <br></br>
                             <span>CSS</span>
                         </article>
                         <article>
-                            <i class="fab fa-python"></i>
+                            <i className="fab fa-python"></i>
                             <br></br>
                             <span>Python</span>
                         </article>
                         <article>
-                            <i class="fab fa-java"></i>
+                            <i className="fab fa-java"></i>
                             <br></br>
                             <span>Java</span>
                         </article>
@@ -63,13 +63,14 @@ export default function Skills() {
                             <span>C</span>
                         </article>
                         <article>
+                            <br></br>
                             <span>C++</span>
                         </article>
                     </div>
                     <div className="v-control">
                         <h1>Version control</h1>
                         <article>
-                            <i class="fab fa-github"></i>
+                            <i className="fab fa-github"></i>
                             <br></br>
                             <span>Github</span>
                         </article>
