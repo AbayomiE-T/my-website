e56aa34031aa95806e384dbab9363dc5d8d8b0d6
Abayomi <eyitouyoa3@gmail.com>
2020-11-17 12:03:56 -0500

    about section revamped

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,87 @@ export default function About() {
             <h1 className="heading">About</h1>
             <div className="rule"></div>
             <div className="content">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident dicta explicabo incidunt hic tempore, soluta possimus alias enim cumque optio minus dolor illum quaerat odit voluptatum recusandae modi expedita autem nobis aliquam facilis? Dolore quos eveniet vel! Harum, quis ex! Perspiciatis voluptatum veniam
-                    laudantium animi vero natus dolore molestiae suscipit dolorem illo? Dolorum veniam consectetur minus, eveniet quisquam in quos et pariatur magni sed quia cupiditate corrupti nulla. Inventore nostrum in excepturi enim saepe eum iure omnis, nam vel natus, qui sequi accusamus, repellat tenetur molestiae quas laborum
-                    corrupti quis minus odio hic. Error illo modi mollitia praesentium sapiente. Sed?
-                </p>
+                <div className="about-me-container">
+                    <div className="about-me">
+                        <span>Image of me</span>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, qui dicta. Id, vitae repudiandae suscipit consequuntur officiis quia voluptatem
+                        voluptates culpa pariatur ratione modi nisi reiciendis, enim ipsam sapiente ullam praesentium commodi ex nesciunt adipisci nulla natus, libero ipsa! Nobis
+                        repudiandae laboriosam eos cumque id officia debitis reiciendis nihil esse quaerat quasi, expedita eligendi rerum rem saepe quae quas, repellendus, incidunt modi.
+                        Magnam harum eos cum odit deserunt, itaque a vitae explicabo enim repellat. Labore inventore, accusantium enim dolore sunt esse cum sed ad pariatur voluptatum
+                        quisquam ipsam quia obcaecati corrupti numquam nobis perspiciatis distinctio fugiat? Nulla quia voluptas ab.
+                    </p>
+                </div>
+
+                <div className="skills">
+                    <article className="skill">
+                        <span className="skill-name">CSS</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">HTML</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Javascript</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">React</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Redux</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Bootstrap</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Django</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Python</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Java</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">C++</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                    <article className="skill">
+                        <span className="skill-name">Github</span>
+                        <div className="percentage">
+                            <span>90%</span>
+                        </div>
+                    </article>
+                </div>
             </div>
         </section>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,4 @@
 import React from 'react'
-import reactIcon from '../icons/react-icon.png'
-import django from '../icons/django.png'
-import bootstrap from '../icons/bootstrap-icon.png'
-import firebase from '../icons/firebase.png'
-import redux from '../icons/redux.png'
-import js from '../icons/js-icon.png'
-import html from '../icons/html-icon.png'
-import css from '../icons/css3-icon.png'
-import java from '../icons/java-icon.png'
-import python from '../icons/python-icon.png'
-import c from '../icons/c.png'
-import cplusplus from '../icons/c_plus_plus.png'
-import git from '../icons/github-icon.png'
-import SkillDetail from './SkillDetail'
 
 export default function Skills() {
     return (
@@ -20,7 +6,7 @@ export default function Skills() {
             <section id="skills">
                 <h1 className="heading">Skills</h1>
                 <div className="rule"></div>
-                <div className="skills-container">
+                {/* <div className="skills-container">
                     <div className="frameworks">
                         <h1>Frameworks & Libraries</h1>
                         <SkillDetail pic={reactIcon} skillName="React" />
@@ -43,7 +29,7 @@ export default function Skills() {
                         <h1>Version control</h1>
                         <SkillDetail pic={git} skillName="Github" />
                     </div>
-                </div>
+                </div> */}
             </section>
         </>
     )
