4c4656e87eca502fdad7d47a25a1ae2f9c63e13c
Abayomi <eyitouyoa3@gmail.com>
2020-12-02 20:06:20 -0500

    Services page fully responsive

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import responsive from '../icons/responsive.png'
+import development from '../icons/development.png'
+import customize from '../icons/customize.png'
+
 export default function Services() {
     return (
         <>
@@ -10,14 +14,14 @@ export default function Services() {
                 <div className="service-container">
                     <div className="service">
                         <div className="service-image-container">
-
+                            <img src={responsive} alt="" />
                         </div>
                         <h1>Responsive</h1>
                         <p>I can make your website compatible with all screens. Even ones as tiny as a pebble.</p>
                     </div>
                     <div className="service">
                         <div className="service-image-container">
-
+                            <img src={development} alt="" />
                         </div>
                         <h1>Development</h1>
                         <p>I like to bring your ideas to life from the ground up using the various languages of web browsers. Got any design mockups? I
@@ -25,7 +29,7 @@ export default function Services() {
                     </div>
                     <div className="service">
                         <div className="service-image-container">
-
+                            <img src={customize} alt="" />
                         </div>
                         <h1>Customize Themes</h1>
                         <p>Have a theme you want tailored to your tastes? I can customize a website theme for you as well.</p>
