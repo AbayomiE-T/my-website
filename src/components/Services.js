83f09125907dacfdddfaa30769a4912f2379ed3a
Abayomi <eyitouyoa3@gmail.com>
2021-01-09 22:13:47 -0500

    fixed a bug on small screen

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,23 +11,25 @@ export default function Services() {
                 <div className="h-colour-2">
                     <h1 className="heading" style={{ color: 'white' }}>Services</h1>
                     <div className="rule" style={{ backgroundColor: 'white' }}></div>
+
+                    <p>I am proficient in HTML, CSS and Javascript and love working with modern frameworks such as React. I even built this website from the ground up using React!</p>
                 </div>
 
                 <div className="service-container">
                     <div className="service">
                         <img src={responsive} alt="" />
-                        <h1>Responsive</h1>
+                        <h2>Responsive</h2>
                         <p>I can make your website compatible with all screens. Even ones as tiny as a pebble.</p>
                     </div>
                     <div className="service">
                         <img src={code} alt="" />
-                        <h1>Development</h1>
+                        <h2>Development</h2>
                         <p>I like to bring your ideas to life from the ground up using the various languages of web browsers. Got any design mockups? I
                         will replicate it with code</p>
                     </div>
                     <div className="service">
                         <img src={theme} alt="" />
-                        <h1>Customize Themes</h1>
+                        <h2>Customize Themes</h2>
                         <p>Have a theme you want tailored to your tastes? I can customize a website theme for you as well.</p>
                     </div>
                 </div>
